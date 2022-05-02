c64240aabbe61aff60c15027b4483fa2f7c4f452
Merwan <myeditha7@gmail.com>
2022-05-02 05:11:59 -0400

    added ground plane

--- src/main.c
+++ src/main.c
@@ -4,23 +4,35 @@
 #include "objects.h"
 #include "vec3.h"
 
-void calc_color(Ray r, Sphere s){
+void calc_color(Ray r, Sphere s, Sphere gnd){
     // right now make a solid color
     //printf("%f %f %f\n",r.dir.x, r.dir.y, r.dir.z);
-    float t = hit_sphere(&r, s.center, s.rad);
+    Vec3 r_normd = get_norm(&(r.dir));
+    Color color = {0,0,0};
+    float t = 0.5*(r_normd.y + 1.0);
+    color.r = (1.0-t)+t*0.5;
+    color.g = (1.0-t)+t*0.7;
+    color.b = (1.0-t)+t*1.0;
+    t = hit_sphere(&r, gnd.center, gnd.rad);
     if (t>0.0) {
         Vec3 r_at = ray_at(r, t);
         r_at.z -= 1;
         norm(&r_at);
-        Color color = {0.5*(r_at.x+1),0.5*(r_at.y+1),0.5*(r_at.z+1)};
-        printf("%d %d %d\n", (int)(color.r*255.999), (int)(color.g*255.999), (int)(color.b*255.999));
-    } else {
-        Vec3 r_normd = get_norm(&(r.dir));
-        float t = 0.5*(r_normd.y + 1.0);
-        Color color = {(1.0-t)+t*0.5, (1.0-t)+t*0.7, (1.0-t)+t*1.0};
-        //printf("not circle\n");
-        printf("%d %d %d\n", (int)(color.r*255.999), (int)(color.g*255.999), (int)(color.b*255.999));
+        color.r = 0;
+        color.g = 1;
+        color.b = 0;
     }
+    t = hit_sphere(&r, s.center, s.rad);
+    if (t>0.0) {
+        Vec3 r_at = ray_at(r, t);
+        r_at.z -= 1;
+        norm(&r_at);
+        color.r = 0.5*(r_at.x+1);
+        color.g = 0.5*(r_at.y+1);
+        color.b = 0.5*(r_at.z+1);
+    }
+    //printf("not circle\n");
+    printf("%d %d %d\n", (int)(color.r*255.999), (int)(color.g*255.999), (int)(color.b*255.999));
 }
 
 int main(){
@@ -45,6 +57,8 @@ int main(){
     //create the sphere
     Vec3 sphere_center = {0,0,-1};
     Sphere s = {&sphere_center, 0.5};
+    Vec3 gnd_center = {0, -100, -1};
+    Sphere gnd = {&gnd_center, 100};
     
     for (int j = image_height-1; j>=0; j--){
         for (int i = 0; i < image_width; i++){
@@ -55,7 +69,7 @@ int main(){
                         lower_left.z+(h.z*u)+(v.z*ve)-origin.z};
             //printf("%f %f %f\n", dir.x, dir.y, dir.z);
             Ray r = {origin, dir};
-            calc_color(r, s);
+            calc_color(r, s, gnd);
         }
     }
 }
--- src/objects.h
+++ src/objects.h
@@ -10,4 +10,10 @@ typedef struct {
     // 0 for no collision, 1 for single root, 2 for two roots
 } Sphere;
 
+typedef struct{
+    void* obj;
+    Color (*get_col)(Ray*, Vec3*, float);
+} Object;
+
 float hit_sphere(Ray*, Vec3*, float);
+Color get_sphere_col(Ray*, Vec3*, float);
